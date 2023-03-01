bad476d52765d15fb3e4400a6b0dbf935b3f2d5f
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-03-01 11:29:04 -0700

    Add ability for user to filter articles based on sport

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,16 +5,21 @@ import ArticleCard from '../ArticleCard/ArticleCard'
 
 const Home = () => {
   const [articles, setArticles] = useState([])
+  const [filteredArticles, setFilteredArticles] = useState([])
+  let allSports = []
 
   useEffect(() => {
     fetch('https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=9GLryxGbI4IPesd7tXkh0KKw9VAuZMNE', {
       method: 'GET'
     })
     .then(response => response.json())
-    .then(data => setArticles(data.results))
+    .then(data => {
+      setArticles(data.results)
+      setFilteredArticles(data.results)
+    })
   }, [])
 
-  const allArticles = articles.map(article => {
+  const allArticles = filteredArticles.map(article => {
     return (
       <ArticleCard
         key={article.created_date}
@@ -25,6 +30,31 @@ const Home = () => {
       />
     )
   })
+  
+  const allOptions = articles.forEach(article => {
+    if(!allSports.includes(article.subsection) && article.subsection !== '') {
+      allSports.push(article.subsection)
+    }
+    return allSports
+  })
+
+  const setOptions = allSports.map(sport => {
+    return (
+      <option value={sport} key={sport}>{sport}</option>
+    )
+  })
+
+  const filterSports = (value) => {
+    if(value === "All Sports") {
+      setFilteredArticles(articles)
+    } else {
+      let display = articles.filter(article => {
+        return article.subsection === value
+      })
+      setFilteredArticles(display)
+    }
+  }
+
 
   return (
     <section className='home-page'>
@@ -32,9 +62,11 @@ const Home = () => {
       <header className='page-header'>
         <h1>Home</h1>
       </header>
+      <h2>Filter By Sport</h2>
       <form className='filter-form'>
-        <select className='filter-option'>
-          <option>Josh Is Him</option>
+        <select className='filter-option' onChange={event => filterSports(event.target.value)}>
+          <option>All Sports</option>
+          {setOptions}
         </select>
       </form>
       <div className='article-container'>
