0a92f9ae173d424f129c33727fbc4e3b3aaf1fd9
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-03-01 10:01:14 -0700

    Add elements to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,19 @@ const Home = () => {
   return (
     <section className='home-page'>
       <Header/>
+      <header className='page-header'>
+        <h1>Home</h1>
+      </header>
+      <form className='filter-form'>
+        <select className='filter-option'>
+          <option>Josh Is Him</option>
+        </select>
+      </form>
+      <div className='article-container'>
+        <h1>Articles go here</h1>
+      </div>
     </section>
   )
 }
 
-export default Home
+export default Home;
