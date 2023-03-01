6521289e37189c1677da367ec665729d440459e8
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-03-01 10:16:02 -0700

    Add basic layout for home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Header from '../Header/Header'
 
 const Home = () => {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=9GLryxGbI4IPesd7tXkh0KKw9VAuZMNE', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => setArticles(data.results))
+  }, [])
+
   return (
     <section className='home-page'>
       <Header/>
