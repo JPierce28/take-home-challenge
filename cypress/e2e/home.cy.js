b21106dffa09c57a0b31508988e431e69df77e2f
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-03-02 17:18:02 -0700

    Add before each for the hoome cypress test

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,9 @@
+escribe('Home Page Test', () => {
+  beforeEach(() => {
+    cy.visit('https://localhost:3000')
+  })
+  cy.intercept('https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=9GLryxGbI4IPesd7tXkh0KKw9VAuZMNE', {
+    method: 'GET',
+    fixture: './home.json'
+  })
+})
