f03ae30f990cf1c1ca58982b08803e688b74531f
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-03-02 17:51:30 -0700

    Add tests for home page html and functionality

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,25 @@
-Describe('Home Page Test', () => {
+describe('Home Page Test', () => {
   beforeEach(() => {
-    cy.visit('https://localhost:3000')
+    cy.visit('http://localhost:3000/')
+    cy.intercept('https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=9GLryxGbI4IPesd7tXkh0KKw9VAuZMNE', {
+      method: 'GET',
+      fixture: '../fixtures/home.json'
+    })
   })
-  cy.intercept('https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=9GLryxGbI4IPesd7tXkh0KKw9VAuZMNE', {
-    method: 'GET',
-    fixture: './home.json'
+  it("Should display the home page with the correct components", () => {
+    cy.get('.home-page').contains('h1', 'Home')
+    cy.get('.global-header').contains('h1', 'NYT Sports')
+    cy.get('.home-page').contains('h2', 'Filter By Sport')
+    cy.get('.filter-option').contains('All Sports')
+    cy.get('.article-card').should('have.length', '2')
+    cy.get('.article-container > :nth-child(1)').contains('p', 'Sport: ncaabasketball')
+    cy.get('.article-container > :nth-child(1)').contains('p', 'He Was Billed as the Next LeBron. But Will Emoni Bates Make It at All?')
+    cy.get('.article-container > :nth-child(1)').contains('p', 'By Billy Witz')
+    cy.get(':nth-child(1) > .image-container > .article-image').should('be.visible')
+  })
+  it("Should allow a user to filter articles by sport", () => {
+    cy.get('.filter-option').select('football')
+    cy.get('.article-card').should('have.length', '1')
+    cy.get('.article-container > :nth-child(1)').contains('p', 'Sport: football')
   })
 })
